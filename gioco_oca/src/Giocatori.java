352731bc05f90bab2df8bfb5bf3d3abc27311663
Cristian Alioto <jokercris18@gmail.com>
2020-11-27 11:56:10 +0100

    demo Giocatori e Gioco

--- gioco_oca/src/Giocatori.java
+++ gioco_oca/src/Giocatori.java
@@ -1,4 +1,84 @@
+import java.util.Arrays;
 
 public class Giocatori {
+
+    private Giocatore[] n = new Giocatore[4];
+    private int counter;
+    private int turno;
+    private int podio;
+
+    public Giocatori(){
+        counter = 0;
+        podio = 0;
+        turno = 0;
+    }
+
+    @Override
+    public String toString() {
+        String giocatori = "Giocatori: " + "\n";
+        for(int i = 0; i<4; i++) giocatori += n[i].getNome() + " | Online: " + n[i].isOnline() + "\n";
+        return giocatori;
+    }
+
+    //Getters
+    public String getTurno(){
+        if(n[turno].isOnline) return "E' il turno di " + n[turno].getNome();
+        aggiornaTurno();
+        return getTurno();
+    }
+
+    public String getPodio(){
+        return "Il giocatore " + n[podio].getNome() + " è sul podio con " + n[podio].getVittorie() + " vittore!";
+    }
+
+    //
+
+    public boolean check(){
+        if(counter>3){
+            for(int i = 0; i<4; i++){
+                if(!n[i].isOnline) return true;
+            }
+            return false;
+        }
+        return true;
+    }
+
+    public String aggiungiGiocatore(String nome){
+        String sost;
+        if(counter<4){
+            n[counter] = new Giocatore(nome);
+            counter++;
+            return "Il giocatore " + nome + " è stato aggiunto.";
+        }
+        for(int i = 0; i<4; i++){
+            if(!n[i].isOnline()){
+                sost = n[i].getNome();
+                n[i] = new Giocatore(nome);
+                return "Il giocatore " + sost + " è stato sostituito da " + nome + ".";
+            }
+        }
+        return null;
+    }
+
+    public String aggiornaPodio(){
+        int max = 0;
+        for(int i = 0; i<3; i++){
+            if(n[i].getVittorie()>n[i+1].getVittorie) max = i;
+            else max = i+1;
+        }
+        return getPodio();
+    }
+
+    public void aggiornaTurno(){
+        if(turno<3) turno++;
+        else turno = 0;
+    }
+
+    public String muovi(){
+        String str;
+        str = n[turno].muovi();
+        aggiornaTurno();
+        return str;
+    }
 
 }
--- gioco_oca/src/Gioco.java
+++ gioco_oca/src/Gioco.java
@@ -1,8 +1,72 @@
 
+import java.util.Scanner;
+
 public class Gioco {
 
-    public static void main(String[] args) {
+    private static Giocatori giocatore = new Giocatori();
+    private Tabellone tab = new Tabellone();
+    private Salvataggio partita = new Salvataggio();
 
+    public static void inizio(){
+        Scanner in = new Scanner(System.in);
+        String nome; char key;
+        System.out.println(" Benvenuto nel gioco dell'oca! ");
+        System.out.println("Premi 'c' per caricare una partita già esistente");
+        System.out.println("premi 'n' per iniziare una nuova partita");
+        do{ key = in.next().charAt(0); }while(key!='n' && key!='c');
+        if(key=='c') partita.carica(giocatore);
+        else{
+            System.out.println("Inserisci il tuo nome: ");
+            do{ nome = in.nextLine(); }while(nome.isEmpty());
+            System.out.println(giocatore.aggiungiGiocatore(nome));
+        }
+        System.out.println("Buon divertimento!" + "\n");
+    }
+
+    public static void menù(){
+        Scanner in = new Scanner(System.in);
+        String nome; char key;
+        System.out.println(" ***             Menù             *** ");
+        System.out.println("Premi 'g' per giocare");
+        System.out.println("Premi 'a' per aggiungere un giocatore");
+        System.out.println("Premi 'm' per mostrare i giocatori in partita");
+        System.out.println("Premi 's' per salvare il gioco");
+        System.out.println("Premi 'e' per uscire dal gioco");
+        System.out.println("premi 'q' per terminare il gioco (per tutti)");
+        do{
+             key = in.next().charAt(0);
+             switch(key){
+                 case 'a' -> {
+                     if(giocatore.check()){
+                         System.out.println("Inserisci il nome del giocatore: ");
+                         do{ nome = in.nextLine(); }while(nome.isEmpty());
+                         giocatore.aggiungiGiocatore(nome);
+                     }
+                     else System.out.println("Limite dei giocatori raggiunto");
+                     menù(); return;
+                 }
+                 case 'm' -> { System.out.println(giocatore); }
+                 case 's' -> { System.out.println(partita.salva(giocatore)); }
+                 case 'e' -> { System.out.println(partita.esci(giocatore)); }
+                 case 'q' -> { System.exit(0); }
+             }
+        }while(key!='g');
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        char key;
+        inizio();
+        menù();
+        do{
+            System.out.print(giocatore.getTurno());
+            System.out.println("Premi 't' per tirare i dadi");
+            System.out.println("Premi 'm' per andare al menù principale");
+            System.out.println("Premi 'q' per terminare il programma (per tutti)");
+            do{ key = in.next().charAt(0); }while(key!='t' && key!='m');
+            if(key=='m') menù();
+            else System.out.println(giocatore.muovi());
+        }while(key!='q');
     }
 
 }
